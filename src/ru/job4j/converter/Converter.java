da9da8236fb22bd2706c4d096704b5ee646bed8d
macbook <79824177721@yandex.ru>
2020-07-10 13:14:15 +0500

    0. Что такое тестирование [#320137]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -15,5 +15,10 @@ public class Converter {
         int dollars = Converter.rubleToDollar(120);
         System.out.println("140 rubles are " + euro + " euro. ");
         System.out.println("120 rubles are " + dollars + " dollars");
+        int in = 120;
+        int expected = 2;
+        int out = rubleToDollar(in);
+        boolean passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
     }
 }
