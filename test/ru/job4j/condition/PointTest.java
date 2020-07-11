2ac537946c6320129f46ac0b982d479c0bcbbffb
macbook <79824177721@yandex.ru>
2020-07-11 07:40:45 +0500

    3. Тесты для определения расстояния между точками. [#320140]

---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        double expected = 1.0;
+        double rsl = Point.distance(0,0,2,0);
+        assertThat(rsl, is(expected));
+
+    }
+}
