615cbddf1f4936be53fbe1e21b407d41d5492021
macbook <79824177721@yandex.ru>
2020-07-13 15:56:01 +0500

    4.3.1. А существует ли треугольник? [#320067]

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public static int max(int first, int second, int third){
+        int max1 = first > second ? first : second;
+        int result = first;
+        return result;
+    }
+}
