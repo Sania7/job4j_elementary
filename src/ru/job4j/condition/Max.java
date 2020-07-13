09e5d0607466175f71bbc00981e9d7beaa1a655b
macbook <79824177721@yandex.ru>
2020-07-13 14:05:44 +0500

    4.2. Тернарное сравнение [#320064]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right){
+        int max = left > right ? left : right;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int number = Max.max(1, 2);
+        System.out.println(number);
+        int num = Max.max(2, 1);
+        System.out.println(num);
+        num = Max.max(2, 2);
+        System.out.println(num);
+    }
+
+}
