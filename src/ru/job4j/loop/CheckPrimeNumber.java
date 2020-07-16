e712d6f05be22b0b24eb4b220ca0690c08343db2
macbook <79824177721@yandex.ru>
2020-07-16 11:08:13 +0500

    5.5. Простое число [#320156]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1 || number <= 0){
+            prime = false;
+        }
+        int num = 2;
+        while (num < number) {
+            if ((number % num) == 0) {
+                prime = false;
+                break;
+            }
+            num++;
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CheckPrimeNumber.check(5));
+    }
+}
+
+
+
+
+
+
+
