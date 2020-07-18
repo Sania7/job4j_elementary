0bd6ca91d7612252162358b3dc1f8de874d3a0bc
macbook <79824177721@yandex.ru>
2020-07-18 15:36:32 +0500

    5.9. Крест в псевдографике. [#320157]

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -1,9 +1,22 @@
 package ru.job4j.live;
 
 public class Loop {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+
+    public static void draw(int size) {
+        for (int x = 0; x <= size; x++) {
+            for (int i = 0; i < size; i++) {
+                
+            }
+            if (x % 2 == 0) {
+                System.out.print("X");
+            } else {
+                System.out.print(" ");
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Строка");
+        draw(10);
     }
 }
