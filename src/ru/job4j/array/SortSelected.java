6971014948aaacd73aefd17870e70da2f8eab201
macbook <79824177721@yandex.ru>
2020-07-22 20:26:26 +0500

    6.6.1. Двухмерный массив. Вывод на консоль. [#320090]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -4,7 +4,7 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexOf(data, min, i, data.length);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
             if (data[i] > min) {
                 int temp = data[i];
                 data[i] = data[index];
@@ -12,5 +12,13 @@ public class SortSelected {
             }
         }
         return data;
+    }
+
+    public static void main(String[] args) {
+        int[] n = {5, 45, 34, 234, 64, 2, -2};
+        int[] rsl = SortSelected.sort(n);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.print(rsl[index] + " ");
+        }
     }
 }
