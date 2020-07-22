254f0f274c106cb2f85efbb8f5bbf085f6238d4b
macbook <79824177721@yandex.ru>
2020-07-22 09:19:03 +0500

    6.4.2. Слово заканчивается на ... [#320076]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,6 +2,12 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        return (word[word.length - 1] == post[post.length - 1]);
+       boolean result = true;
+        for (int index = 0; index < post.length; index++) {
+            if (word[word.length - index - 1] != post[post.length - index - 1]) {
+                return false;
+            }
+        }
+        return result;
     }
 }
