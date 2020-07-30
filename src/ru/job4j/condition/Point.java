ba2784135bce258c8afe0e7c7c44163904510d68
macbook <79824177721@yandex.ru>
2020-07-30 05:23:01 +0500

    2.1. Рефакторинг - Расстояние между точками. [#320235]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,16 +6,13 @@ import static java.lang.Math.sqrt;
 public class Point {
     private int x;
     private int y;
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
      Point a = new Point(0, 0);
      Point b = new Point(0, 2);
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,16 +1,17 @@
-//package ru.job4j.condition;
-//
-//import org.junit.Test;
-//import static org.junit.Assert.assertThat;
-//import static org.hamcrest.Matchers.is;
-//import static org.junit.Assert.*;
-//
-//public class PointTest {
-//    @Test
-//    public void distance() {
-//        double expected = 2.0;
-//        double rsl = Point.distance(0,0,2,0);
-//        assertThat(rsl, is(expected));
-//
-//    }
-//}
+package ru.job4j.condition;
+
+import org.junit.Test;
+import java.awt.Point;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    @Test
+    public void distance() {
+        double expected = 2.0;
+        Point a = new Point(0,0);
+        Point b = new Point(0,2);
+        double dist = a.distance(b);
+        assertThat(dist, is(expected));
+    }
+}
