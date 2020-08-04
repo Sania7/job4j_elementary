2cdbe532071cd200614f4b7781c378a288d2e1cf
macbook <79824177721@yandex.ru>
2020-08-04 12:37:10 +0500

    5. Аннотация @Override [#320270]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,8 +1,9 @@
 package ru.job4j.inheritance;
 
 
+
+    public class JSONReport extends TextReport {
         @Override
-        public class JSONReport extends TextReport{
         public String generate(String name, String body) {
         return "{ " + System.lineSeparator() + name + " name" + "," + System.lineSeparator() + body + " body" + System.lineSeparator() + "}";
     }
