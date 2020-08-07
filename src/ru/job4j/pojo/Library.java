9c39cd4088f45b994c6f9b887dc12d1a4beaa714
macbook <79824177721@yandex.ru>
2020-08-07 21:34:02 +0500

    3. Массивы и модели. [#320277]

---
+++ src/ru/job4j/pojo/Library.java
@@ -0,0 +1,29 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book = new Book("Clean code", 450);
+        Book book1 = new Book("Chess", 470);
+        Book book2 = new Book("Algorithms", 550);
+        Book book3 = new Book("Regular expressions", 600);
+        Book[] books = new Book[4];
+        books[0] = book;
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+        for (int index = 0; index < books.length; index++) {
+            Book bks = books[index];
+            System.out.println(bks.getName() + " - " + bks.getNumberOfPages() + " страниц.");
+        }
+        System.out.println();
+        System.out.println("Поменять местами индексы 0 и 3");
+        System.out.println();
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getNumberOfPages() + " страниц.");
+        }
+    }
+}
