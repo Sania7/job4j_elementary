d4cd77765584a17dcb0daf4cd1f5933a1870b529
macbook <79824177721@yandex.ru>
2020-08-10 12:29:51 +0500

    3.2. Удаление моделей из массива. [#320283]

--- src/ru/job4j/pojo/ShopDrop.java
+++ src/ru/job4j/pojo/ShopDrop.java
@@ -33,7 +33,7 @@ public class ShopDrop {
         }
     }
     public static Product[] leftShift(Product[] products, int index) {
-        for (int i = index; i < products.length - 1; index++) {
+        for (int i = index; i < products.length - 1; i++) {
             products[index] = products[index + 1];
         }
             products[products.length - 1] = null;
