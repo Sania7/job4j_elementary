a8c33ab12121c01d0b36460b3bac8b64b7062528
macbook <79824177721@yandex.ru>
2020-08-17 12:20:16 +0500

    2. Scanner и чтение числа из консоли. [#320205]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -4,18 +4,34 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-        int remain = 0;
         int amount = 11;
-        while (amount > remain) {
-            System.out.println("Player, take one to three matches ;) ");
+        while (amount > 0) {
+            System.out.println("Player 1, take one to three matches!");
             Scanner sc = new Scanner(System.in);
-            int number = sc.nextInt();
-            System.out.println("You took " + number + ".");
-            amount = amount - number;
-            System.out.println(amount + " matches left.");
-            if (amount == remain) {
-                System.out.println("Congratulations, you won!");
-                break;
+            int number1 = sc.nextInt();
+            if (number1 >= 4 || number1 <= 0) {
+                System.out.println("You took the wrong amount!");
+            } else {
+                amount = amount - number1;
+                System.out.println(amount + " matches left.");
+                if (amount == 0) {
+                    System.out.println("Congratulations, you won!");
+                } else {
+
+                    System.out.println("Player 2, take one to three matches!");
+                    Scanner scn = new Scanner(System.in);
+                    int number2 = scn.nextInt();
+                    if (number2 >= 4 || number2 <= 0) {
+                        System.out.println("You took the wrong amount!");
+
+                    } else {
+                        amount = amount - number2;
+                        System.out.println(amount + " matches left.");
+                        if (amount == 0) {
+                            System.out.println("Congratulations, you won!");
+                        }
+                    }
+                }
             }
         }
     }
