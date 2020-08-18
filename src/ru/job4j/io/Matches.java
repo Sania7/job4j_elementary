6b7ede85d9551ed228f03f2e275173e905244670
macbook <79824177721@yandex.ru>
2020-08-18 14:01:33 +0500

    2. Scanner и чтение числа из консоли. [#320205]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -5,26 +5,25 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         int amount = 11;
-        boolean prime = true;
-        String name = " ";
-        name = player ? "игрок_1" : "игрок_2";
+        boolean player = true;
+        String name = "";
         while (amount > 0) {
-
-            System.out.println("Player 1, take one to three matches!");
+            name = player ? "Player 1" : "Player 2";
+            System.out.println(name + " , take one to three matches!");
             Scanner sc = new Scanner(System.in);
             int number1 = sc.nextInt();
             if (number1 >= 4 || number1 <= 0) {
                 System.out.println("You took the wrong amount!");
             } else {
                 amount = amount - number1;
+                if (amount == 0) {
+                    break;
+                }
                 System.out.println(amount + " matches left.");
-
-            }
-            if (amount == 0) {
-                System.out.println("Congratulations, you won!");
-                break;
+                player = !player;
             }
         }
-
+        System.out.println("Congratulations," + player + " won!");
     }
 }
+
