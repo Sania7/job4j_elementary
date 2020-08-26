74447638c006804d1baac68de390989dcb1c305e
macbook <79824177721@yandex.ru>
2020-08-26 09:21:17 +0500

    7. Шаблон проектирования - Стратегия [#320218]

---
+++ src/ru/job4j/strategy/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return     " __________ " + ln
+                +  "|          |" + ln
+                +  "|          |" + ln
+                +  "|          |" + ln
+                +  "|          |" + ln
+                +  " ---------- " + ln;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Square().draw());
+    }
+}
