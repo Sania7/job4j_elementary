a402429a33387e677464d55e5843bd0c71ca027a
macbook <79824177721@yandex.ru>
2020-09-02 20:44:20 +0500

    0.6. Error - исключения, связанные с работой виртуальной машины [#320228]

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -1,8 +1,8 @@
 package ru.job4j.array;
 
-public class Matrix  {
-     @SuppressWarnings("checkstyle:EmptyLineSeparator")
-     public static int[][] multiple(int size) {
+public class Matrix {
+
+    public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
         for (int i = 0; i < table.length; i++) {
             for (int k = 0; k < table.length; k++) {
--- src/ru/job4j/array/MatrixSum.java
+++ src/ru/job4j/array/MatrixSum.java
@@ -7,7 +7,6 @@ public class MatrixSum {
             for (int j = 0; j < array[i].length; j++) {
                 rsl += array[i][j];
             }
-
         }
         return rsl;
     }
