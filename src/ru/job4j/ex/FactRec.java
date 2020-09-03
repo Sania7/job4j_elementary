b0e944851692b6f576da9336c7c274a10aa4cf5c
macbook <79824177721@yandex.ru>
2020-09-03 13:34:10 +0500

    0.6. Error - исключения, связанные с работой виртуальной машины [#320228]

--- src/ru/job4j/ex/FactRec.java
+++ src/ru/job4j/ex/FactRec.java
@@ -2,13 +2,13 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        return calc(n - 1) * n;
         if ((n == 0) || (n == 1)) {
             return 1;
         } else {
             return calc(n - 1) * n;
         }
     }
+
 
 
     public static void main(String[] args) {
